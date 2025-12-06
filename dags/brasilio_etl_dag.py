c94e27774eea6bfae9387b25548e2307aa3251f4
Ivo Junior <icaj@cesar.school>
2025-12-06 12:23:45 -0300

    ajustes das dags e nomes dos arquivos de jobs

--- dags/brasilio_etl_dag.py
+++ dags/brasilio_etl_dag.py
@@ -1,38 +1,45 @@
+import os
+import sys
+
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from jobs.collect_raw import run as collect_raw_run
-from jobs.raw_to_bronze import run as raw_to_bronze_run
-from jobs.bronze_to_silver import run as bronze_to_silver_run
-from jobs.silver_to_gold_duck import run as silver_to_gold_duck_run
+BRASILIO_PATH="/home/ivo/Documentos/brasilio/BrasilIO-1/"
+if BRASILIO_PATH not in sys.path:
+    sys.path.append(BRASILIO_PATH)
 
+from jobs.download_raw import run as download_raw_run
+from jobs.raw_para_bronze import run as raw_para_bronze_run
+from jobs.bronze_para_silver import run as bronze_para_silver_run
+from jobs.silver_para_gold_duck import run as silver_para_gold_duck_run
+   
 with DAG(
     dag_id="brasilio_etl",
     start_date=datetime(2025, 1, 1),
-    schedule_interval="@daily",  # ou @monthly, etc.
+    schedule="@hourly",  # ou @monthly, etc.
     catchup=False,
     tags=["brasil-io", "engenharia-dados"],
 ) as dag:
 
     t_raw = PythonOperator(
-        task_id="collect_raw",
-        python_callable=collect_raw_run,
+        task_id="download_raw",
+        python_callable=download_raw_run,
     )
 
     t_bronze = PythonOperator(
-        task_id="raw_to_bronze",
-        python_callable=raw_to_bronze_run,
+        task_id="raw_para_bronze",
+        python_callable=raw_para_bronze_run,
     )
 
     t_silver = PythonOperator(
-        task_id="bronze_to_silver",
-        python_callable=bronze_to_silver_run,
+        task_id="bronze_para_silver",
+        python_callable=bronze_para_silver_run,
     )
 
     t_gold_duck = PythonOperator(
-        task_id="silver_to_gold_duck",
-        python_callable=silver_to_gold_duck_run,
+        task_id="silver_para_gold_duck",
+        python_callable=silver_para_gold_duck_run,
     )
 
     # Orquestração
